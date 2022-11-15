f44ae98eac136b605e369455afd27648cbbcd520
SharonBeth <107509578+SharonBeth@users.noreply.github.com>
2022-11-15 13:17:05 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ function getGiph(res) {
     if(numResults) {
         let randomInd = Math.floor(Math.random() * numResults);
         console.log('This is inside teh if statements')
-        let $newCol = $("div", {class: "col-md-4 col-12 mb-4"});
+        let $newCol = $("<div>", {class: "col-md-4 col-12 mb-4"});
         let $newGiph = $("<img>", {
             src: res.data[randomInd].images.original.url,
             class: "w-100"
         })
+        let $title = $("<div>", {text: "testing the div words", class: "text-center"})
+        //  {innerText : res.data[randomInd].title})
         $newCol.append($newGiph);
         $giphArea.append($newCol);
+        console.log($title)
+        $newCol.append($title)
        
     }
 }
@@ -36,8 +40,27 @@ $('form').on('submit', async function (evt){
 })
 
 
-async function test() {
-    const waka = await axios.get('http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym')
-    console.log(waka);
-  }
+
+
+
+
+// Used this to test to confirm I was able to call into the API without function coding. I was having trouble at one point, and was getting a 404 error.
+// async function test() {
+    // const waka = await axios.get('http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym')
+    // console.log(waka);
+//   }
+// 
+// 
+
+
+//////////////////////////////////////
+//Questions for Wednesday           //
+//////////////////////////////////////
+
+
+
+
+//The code for line 16 and 17 seems like it should be flip-flopped for order. I thought it would work this way, because if you append $newGiph to $newCol its not there yet until the next line.?
+
+//Tried to add a div with the caption of the title to the gif. I was not successful, but i have some thoughts on it: use a div, then use a variable, but is .innertext of a div usable? I couldn't get it to work.
 
